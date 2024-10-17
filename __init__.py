a8155802fd5a4b731e316627ff3342365d4a27ae
Eugene <eugenevasile@gmail.com>
2024-10-17 17:29:03 +0200

    Repo init
    Repo init

---
+++ __init__.py
@@ -0,0 +1,35 @@
+import json  # Import the json module to work with JSON files
+
+
+food_list = [
+    (
+        "Cornstarch",
+        {
+            "calories": 381,
+            "total_fat": 0.1,
+            "protein": 0.26,
+            "carbohydrate": 91.27,
+            "sugars": 0.0,
+        },
+    ),
+    (
+        "Nuts, pecans",
+        {
+            "calories": 691,
+            "total_fat": 72.0,
+            "protein": 9.17,
+            "carbohydrate": 13.86,
+            "sugars": 3.97,
+        },
+    ),
+    (
+        "Eggplant, raw",
+        {
+            "calories": 25,
+            "total_fat": 0.2,
+            "protein": 0.98,
+            "carbohydrate": 5.88,
+            "sugars": 3.53,
+        },
+    ),
+]
